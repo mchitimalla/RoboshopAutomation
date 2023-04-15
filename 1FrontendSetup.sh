b06347f3e620dde1b01077d6b6dab190aac55a1a
Monika <monika.chitimalla@gmail.com>
2023-04-15 18:32:03 +0100

    Bug fix

--- 1FrontendSetup.sh
+++ 1FrontendSetup.sh
@@ -1,6 +1,7 @@
 yum install nginx -y
 rm -rf /usr/share/nginx/html/*
 cp roboshop.conf /etc/nginx/default.d/
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 systemctl enable nginx
