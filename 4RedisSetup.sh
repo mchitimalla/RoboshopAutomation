ded3dd726e38d572544d956cda935f0a6764bcc6
Monika <monika.chitimalla@gmail.com>
2023-04-23 15:21:56 +0100

    Bug fix

--- 4RedisSetup.sh
+++ 4RedisSetup.sh
@@ -12,7 +12,7 @@ yum install redis -y &>>$logfile
 statusCheck $?
 printHeader "Update Redis Listen Address"
 #Update listening address to 0.0.0.0 in /etc/redis.conf
-sed -i 's|127.0.0.1|0.0.0.0|' /etc/redis.conf &>>$logfile
+sed -i 's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf &>>$logfile
 statusCheck $?
 funcEnableService
 statusCheck $?
