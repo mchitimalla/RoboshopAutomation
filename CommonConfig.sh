c595af6ec3a5e57d89c5d84d753b005c26652f18
Monika <monika.chitimalla@gmail.com>
2023-04-23 15:36:50 +0100

    Bug fix

--- CommonConfig.sh
+++ CommonConfig.sh
@@ -1,7 +1,7 @@
 frontend_code_url=https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 mongod_url=mongodb.eroboshop.online
 mysql_url=mysql.eroboshop.online
-mysql_user=root
+mysql_user="root"
 mysql_pwd="RoboShop@1"
 app_user="roboshop"
 rabbitmq_user="roboshop"
